b2cc15ef444c215da5c9ed70aae0fbe055621daa
Nathan <nathan.pignon79@gmail.com>
2025-02-07 12:25:12 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TitleComponent} from './components/title/title.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'nathanpignon';
 }
